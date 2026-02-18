ecb4c47c53e6fb89ae4602255ba6a1a95496c49f
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-18 20:03:30 +0600

    style transfer section

---
+++ script/transfer.js
@@ -0,0 +1,31 @@
+
+
+document.getElementById('transfer-btn')
+    .addEventListener('click', function(){
+        const tranferNumber = getValueFromInput('transfer-number');
+        if(tranferNumber.length !== 11){
+            alert('Invalid Number');
+            return;
+        }
+        const transferAmount = getValueFromInput('transfer-amount');
+        const currentBalance = getBalance();
+        const newBalance = currentBalance - Number(transferAmount);
+        console.log(newBalance);
+        if(newBalance < 0 ){
+            alert('Invalid Amount');
+            return;
+        }
+        const transferPin = getValueFromInput('transfer-pin');
+        if( transferPin === '1234'){
+            alert('Transfer Successful');
+            setBalance(newBalance);
+            const history = document.getElementById('history-container');
+            const newHistory = document.createElement('div');
+            newHistory.innerHTML = `
+            <div class="Transaction-card p-5 bg-base-100">
+            Transfer $${transferAmount} Success to ${tranferNumber} at ${new Date()}
+            </div>
+        `;
+        history.append(newHistory);
+        }
+})
