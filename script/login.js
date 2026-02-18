ace26e5f67fd64ef73ea85952cfe4bfa1e087388
nusratjahan7 <nusratjahan77222@gmail.com>
2026-02-18 21:12:08 +0600

    fix

--- script/login.js
+++ script/login.js
@@ -12,8 +12,8 @@ document.getElementById('login-btn').addEventListener('click', function(){
     if(contactNumber == "01234567890" && pin == "1234"){
         // 3-1. true:::>> alert> homepage
         alert("Login Successful");
-        window.location.replace("/home.html");
-        window.location.assign("/home.html");
+        // window.location.replace("./home.html");
+        window.location.assign("./home.html");
     }
     // 3-2. false:::>> alert> return
     else{
